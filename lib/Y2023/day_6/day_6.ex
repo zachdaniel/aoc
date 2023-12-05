6adffbf8646977ab3f2760333fc1905a14d01a21
Zach Daniel <zach@zachdaniel.dev>
2023-12-05 10:49:56 -0500

    chore: add day 6 empty, to test encryption

---
+++ lib/Y2023/day_6/day_6.ex
@@ -0,0 +1,24 @@
+defmodule Aoc.Y2023.Day6 do
+  use Aoc.Day
+
+  answers do
+  end
+
+  input do
+    use_example? true
+
+    handle_input fn input ->
+      input
+    end
+  end
+
+  solutions do
+    part_1 fn input ->
+      input
+    end
+
+    part_2 fn input ->
+      input
+    end
+  end
+end
